522a4d40359baacf759429280ea1706acfe696e4
Oliver <Dream_sec@163.com>
2023-03-04 21:33:23 +0800

    Zookeeper服务注册与发现

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextBean.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Oliver
+ * @create 2023-03-04 21:15
+ */
+@Configuration
+public class ApplicationContextBean {
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
